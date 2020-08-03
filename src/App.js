ba9cc2e15aad58533c03326075282449303a70c0
Rob <rbhouck32@gmail.com>
2020-08-03 18:27:21 -0400

    This commit is the completion of MVP on this project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { ProductContext } from "../src/contexts/ProductContext"
+import { CartContext } from "../src/contexts/CartContext"
 
 function App() {
 	const [products] = useState(data);
@@ -13,20 +15,26 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
+		console.log("rh: app.js:", cart);
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value= {{ cart, products, addItem}}>
+				<CartContext.Provider value= {{cart}}>
+			<Navigation />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products/>
 			</Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart/>
 			</Route>
+			</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
